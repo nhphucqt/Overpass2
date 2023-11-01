9997f5d0cf9b7cd7ae04d4c4dece9bbd178dcc1f
Shibainu13 <shibainu.auth@gmail.com>
2023-11-01 19:38:04 +0700

    chore: fix Type error with MusicPlayer::play

--- src/framework/MusicPlayer.cpp
+++ src/framework/MusicPlayer.cpp
@@ -1,32 +1,25 @@
 #include "MusicPlayer.hpp"
 
-MusicPlayer::MusicPlayer()
-    : mMusic(), mFilenames(), mVolume(100.0f)
-{
-    // Example
-    // mFilenames[MusicID::MenuTheme] = "[path to menu theme]";
-    // mFilenames[MusicID::MissionTheme] = "[path to mission theme]";
+MusicPlayer::MusicPlayer() : mMusic(), mFilenames(), mVolume(100.0f) {
+  // Example
+  // mFilenames[MusicID::MenuTheme] = "[path to menu theme]";
+  // mFilenames[MusicID::MissionTheme] = "[path to mission theme]";
 }
 
-void MusicPlayer::play(MusicId theme)
-{
-    std::string filename = mFilenames[theme];
-    if (!mMusic.openFromFile(filename))
-        throw std::runtime_error("Music " + filename + " could not be loaded.");
-    mMusic.setVolume(mVolume);
-    mMusic.setLoop(true);
+void MusicPlayer::play(MusicID theme) {
+  std::string filename = mFilenames[theme];
+  if (!mMusic.openFromFile(filename))
+    throw std::runtime_error("Music " + filename + " could not be loaded.");
+  mMusic.setVolume(mVolume);
+  mMusic.setLoop(true);
+  mMusic.play();
+}
+
+void MusicPlayer::stop() { mMusic.stop(); }
+
+void MusicPlayer::setPaused(bool paused) {
+  if (paused)
+    mMusic.pause();
+  else
     mMusic.play();
-}
-
-void MusicPlayer::stop()
-{
-    mMusic.stop();
-}
-
-void MusicPlayer::setPaused(bool paused)
-{
-    if (paused)
-        mMusic.pause();
-    else
-        mMusic.play();
 }
