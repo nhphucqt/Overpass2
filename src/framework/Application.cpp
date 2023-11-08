d850f3870a2e09397d325f6af6f44de6ba5cc8d8
Shibainu13 <shibainu.auth@gmail.com>
2023-11-08 14:06:10 +0700

    feat: AppConfig in Singleton pattern

--- src/framework/Application.cpp
+++ src/framework/Application.cpp
@@ -4,13 +4,15 @@
 #include <Application.hpp>
 
 Application::Application(int windowWidth, int windowHeight, std::string windowTitle)
-: mWindow(sf::VideoMode(800, 600), "Application") {}
+    : mWindow(sf::VideoMode(windowWidth, windowHeight), "Application") {}
 
-void Application::addLauncher(Activity::Ptr launcher, Intent::Ptr intent) {
+void Application::addLauncher(Activity::Ptr launcher, Intent::Ptr intent)
+{
     activityManager.startActivity(std::move(launcher), std::move(intent));
 }
 
-void Application::addLauncher(Activity::Ptr launcher) {
+void Application::addLauncher(Activity::Ptr launcher)
+{
     activityManager.startActivity(std::move(launcher), Intent::Builder().build());
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Application.hpp>
 #include <DemoActivity.hpp>
+#include <AppConfig.hpp>
+#include <ResourceManager.hpp>
 
 int main()
 {
-  Application app(800, 600, "Application");
+  std::cout << AppConfig::getInstance().get<std::string>(ConfigKey::AppName) << std::endl;
+  std::pair<int, int> windowSize = AppConfig::getInstance().get<std::pair<int, int>>(ConfigKey::WindowSize);
+  std::cout << windowSize.first << ' ' << windowSize.second << std::endl;
+
+  Application app(windowSize.first, windowSize.second, "Application");
   Activity::Ptr launcher = std::make_unique<DemoActivity>();
   app.addLauncher(std::move(launcher));
   app.run();
