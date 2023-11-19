abd13ad3793cb931ddee80c30b7ca584a8d32d7a
Shibainu13 <shibainu.auth@gmail.com>
2023-11-19 20:55:46 +0700

    chores: move data table to data folder

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,17 @@
 
 int main()
 {
-  // AppConfig &config = AppConfig::getInstance();
-  // std::pair<int, int> windowSize = config.get<std::pair<int, int>>(ConfigKey::WindowSize);
-  // std::string windowTitle = config.get<std::string>(ConfigKey::AppName);
+  AppConfig &config = AppConfig::getInstance();
+  std::pair<int, int> windowSize = config.get<std::pair<int, int>>(ConfigKey::WindowSize);
+  std::string windowTitle = config.get<std::string>(ConfigKey::AppName);
 
-  // std::cout << windowTitle << std::endl;
-  // std::cout << windowSize.first << ' ' << windowSize.second << std::endl;
+  std::cout << windowTitle << std::endl;
+  std::cout << windowSize.first << ' ' << windowSize.second << std::endl;
 
-  // Application app(windowSize.first, windowSize.second, windowTitle);
-  // Activity::Ptr launcher = std::make_unique<DemoActivity>();
-  // app.addLauncher(std::move(launcher));
-  // app.run();
+  Application app(windowSize.first, windowSize.second, windowTitle);
+  Activity::Ptr launcher = std::make_unique<DemoActivity>();
+  app.addLauncher(std::move(launcher));
+  app.run();
 
   UserRepo &userRepo = UserRepo::getInstance();
   // userRepo.addUser(UserData("john doe", "123", 2300));
