89ff8dcdbdefd2780b74c0cf3178f8676365c0e3
idirius <122882721+idirius@users.noreply.github.com>
2023-12-07 21:32:09 +0700

    remove Obstacle, rewrite Vehicle

--- src/entity/Vehicle.cpp
+++ src/entity/Vehicle.cpp
@@ -1,29 +1,18 @@
 #include <Vehicle.hpp>
 
-Vehicle::Vehicle(sf::Texture texture): Entity(texture), interval(sf::seconds(INTERVAL)), health(1) {}
+TextureID toTextureID(Vehicle::Type type) {
+	switch (type) {
+	case Vehicle::Car:
+		return TextureID::Car;
 
-Vehicle::Vehicle(sf::Texture texture, float t): Entity(texture), interval(sf::seconds(t)), health(1) {}
-
-Vehicle::Vehicle(sf::Texture texture, int hp): Entity(texture), interval(sf::seconds(INTERVAL)), health(hp) {}
-
-bool Vehicle::isRidden(Entity& other) {
-    return checkCollision(other) && this->getGlobalBounds().contains(other.center());
+	// other vehicles here
+	}
+	return TextureID::Car;
 }
 
-void Vehicle::move() {
-    sf::Time timePerFrame = sf::seconds(1.f/(float) FPS);
-    Entity::move(timePerFrame);
-}
-
-bool Vehicle::isDestroyed() {
-    return health <= 0 && interval <= sf::Time::Zero;
-}
-
-bool Vehicle::collides() {
-    --health;
-    return isDestroyed();
-}
-
-void Vehicle::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const {
-	target.draw(sprite, states);
+Vehicle::Vehicle(Type mType, const TextureManager& textures): 
+type(mType), 
+Entity(textures.get(toTextureID(type))) {
+	sf::FloatRect bounds = sprite.getLocalBounds();
+	sprite.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
 }
