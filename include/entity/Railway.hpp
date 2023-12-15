968d00c63096cb0e779f6df590d0a9fae1c2df31
idirius <122882721+idirius@users.noreply.github.com>
2023-12-15 22:51:49 +0700

    add Train and Railway

---
+++ include/entity/Railway.hpp
@@ -0,0 +1,22 @@
+#ifndef RAILWAY_HPP
+#define RAILWAY_HPP
+#include <Train.hpp>
+#include <Lane.hpp>
+#include <ResourceID.hpp>
+#include <ResourceManager.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class Railway: public Lane {
+public:
+    Railway(TextureManager* textures, ViewGroup* foreground, bool isReverse = false);
+
+private:
+	Train* train;
+    ViewGroup* foreground;
+    void updateCurrent(sf::Time dt);
+    void buildLane();
+};
+
+#endif
---
+++ src/entity/Railway.cpp
@@ -0,0 +1,38 @@
+#include <Railway.hpp>
+
+Railway::Railway(TextureManager *textures, ViewGroup* foreview, bool isReverse):
+Lane(textures->get(TextureID::Rail), textures, isReverse), foreground(foreview) {
+    type = Lane::Type::Railway;
+    textures->get(TextureID::Rail).setRepeated(true);
+    sprite.scale(8.f, 8.f);
+	sf::IntRect textureRect(0, 0, 1400, 16);
+    sprite.setTextureRect(textureRect);
+    buildLane();
+}
+
+void Railway::updateCurrent(sf::Time dt) {
+    if (isReverse) {
+        if (train->getPosition().x < -1000)
+            train->setPosition(train->getPosition().x + 2400 + train->getGlobalBounds().width, train->getPosition().y); 
+    }
+    else {
+        if (train->getPosition().x > 2400)
+            train->setPosition(train->getPosition().x - 3400, train->getPosition().y); 
+    }
+}
+
+void Railway::buildLane() {
+    std::unique_ptr<Train> mTrain(new Train(*laneTextures));
+    mTrain->scale(0.5, 0.5);
+    train = mTrain.get();
+    if (isReverse) {
+        mTrain->scale(-1.f, 1.f);
+        mTrain->setPosition(1400.f, 96.f);
+        mTrain->setVelocity(-700.f, 0.f);
+    }
+    else {
+        mTrain->setPosition(0.f, 96.f);
+        mTrain->setVelocity(700.f, 0.f);
+    }
+    this->attachView(std::move(mTrain)); 
+}
