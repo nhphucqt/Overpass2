c7e872c66c0221ebc48270deccdbc5d907709ab0
Shibainu13 <shibainu.auth@gmail.com>
2024-01-10 14:19:31 +0700

    feat: set executable path for macos

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,25 @@
 #include <UserSession.hpp>
 #include <UtilitySave.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
 	unsigned int seed = std::time(nullptr);
 	std::cerr << "Seed: " << seed << std::endl;
 	std::srand(seed);
 
+	// Find the absolute path of the executable
+	std::filesystem::path executablePath = std::filesystem::canonical(std::filesystem::path(argv[0]).parent_path());
+
+	// Set the working directory to the executable's directory
+	std::filesystem::current_path(executablePath);
+
 	AppConfig &config = AppConfig::getInstance();
 	sf::Vector2f windowSize = config.get<sf::Vector2f>(ConfigKey::WindowSize);
 	std::string windowTitle = config.get<std::string>(ConfigKey::AppName);
 
-
 	std::string dataPath = config.get<std::string>(ConfigKey::DATA_PATH);
 	std::error_code err;
-    UtilitySave::CreateDirectoryRecursive(dataPath, err);
+	UtilitySave::CreateDirectoryRecursive(dataPath, err);
 
 	Application app(windowSize.x, windowSize.y, windowTitle);
 	Activity::Ptr launcher = std::make_unique<MainMenuActivity>();
